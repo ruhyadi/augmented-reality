132e6296d25a683fe6f2d5db6a7cff467feb2fbc
Didi Ruhyadi <ruhyadi.dr@gmail.com>
2022-02-24 13:25:38 +0700

    fix interactive mode

--- runDockerCompose.sh
+++ runDockerCompose.sh
@@ -2,5 +2,5 @@ xhost +local:docker
 XSOCK=/tmp/.X11-unix
 XAUTH=/tmp/.docker.xauth
 xauth nlist $DISPLAY | sed -e 's/^..../ffff/' | xauth -f $XAUTH nmerge -
-docker-compose up
+docker-compose run augmented-reality
 xhost -local:docker
